be1bc1b5195aa49cd9eadfe87903c87de7e1ddbc
mrlee <ducmta270897@gmail.com>
2025-01-21 22:31:58 +0700

    [dev] update name screen

---
+++ src/main/java/com/thymeleaf/demo/controller/S1002_TestingPage/s1002_TestingHelper.java
@@ -0,0 +1,75 @@
+package com.thymeleaf.demo.controller.S1002_TestingPage;
+
+import com.thymeleaf.demo.dto.s1002.s1002_ListQuestionForm;
+import com.thymeleaf.demo.dto.s1002.s1201_QuestionDto;
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class s1002_TestingHelper {
+
+    public s1002_ListQuestionForm initScreen(){
+
+        List<s1201_QuestionDto > questions = new ArrayList<>();
+
+        questions.add(
+                s1201_QuestionDto.builder()
+                        .id(1)
+                        .image("/default_image.jpg")
+                        .question("1.I don't have time for u")
+                        .anserts(
+                                Map.of(
+                                        "1","dap an 1",
+                                        "2","dap an 2",
+                                        "3","dap an 3",
+                                        "4","dap an 4",
+                                        "5","dap an 5",
+                                        "6","dap an 6"
+                                )
+                        )
+                        .build()
+        );
+
+        questions.add(
+                s1201_QuestionDto.builder()
+                        .id(2)
+                        .image("/default_image.jpg")
+                        .question("2.I don't have time for u")
+                        .anserts(
+                                Map.of(
+                                        "1","dap an 1",
+                                        "2","dap an 2",
+                                        "3","dap an 3"
+                                )
+                        )
+                        .build()
+        );
+
+        questions.add(
+                s1201_QuestionDto.builder()
+                        .id(1)
+                        .image("/default_image.jpg")
+                        .question("2.I don't have time for u")
+                        .anserts(
+                                Map.of(
+                                        "1","dap an 1",
+                                        "2","dap an 2",
+                                        "3","dap an 3"
+                                )
+                        )
+                        .build()
+        );
+
+        s1002_ListQuestionForm form = s1002_ListQuestionForm.builder()
+                .beginTime(new Date(System.currentTimeMillis()))
+                .endTime(new Date(System.currentTimeMillis() + 60*60*1000))
+                .questions(questions)
+                .build();
+
+        return form;
+    }
+}
