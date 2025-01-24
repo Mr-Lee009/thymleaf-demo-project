d17eb774ae7f0f47385ea0dd490ff6da9e218c28
mrlee <ducla@gmail.com>
2025-01-24 14:13:33 +0700

    update SQL

---
+++ src/main/resources/dev/db/migration/V2__add_table_rule.sql
@@ -0,0 +1,17 @@
+CREATE TABLE RULE(
+    id int primary key,
+    rule_name nvarchar(1024),
+    create_by nvarchar(1024),
+    update_by nvarchar(1024),
+    create_time timestamp default current_timestamp,
+    update_time timestamp default current_timestamp
+);
+
+CREATE TABLE RULE2(
+                     id int primary key,
+                     rule_name nvarchar(1024),
+                     create_by nvarchar(1024),
+                     update_by nvarchar(1024),
+                     create_time timestamp default current_timestamp,
+                     update_time timestamp default current_timestamp
+);
