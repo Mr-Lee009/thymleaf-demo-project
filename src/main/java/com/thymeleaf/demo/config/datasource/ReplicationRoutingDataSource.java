fae3028e9f5bdc6c326d939f8577c1d5fe5d7437
mrlee <ducla@gmail.com>
2025-04-21 17:06:13 +0700

    [IS] update config

--- src/main/java/com/thymeleaf/demo/config/datasource/ReplicationRoutingDataSource.java
+++ src/main/java/com/thymeleaf/demo/config/datasource/ReplicationRoutingDataSource.java
@@ -3,6 +3,10 @@ package com.thymeleaf.demo.config.datasource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
+  /**
+   * Use ThreadLocal to make sure every thread has its own data source context
+   * which won't be changed by other threads
+   */
   private static final ThreadLocal<String> CONTEXT = new ThreadLocal<>();
   public static void setRead() {
     CONTEXT.set("READ");
